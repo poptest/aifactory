3b61fbef259224f2e561cd0cf870487b2e0a2bac
caoxunmo <958022706@qq.com>
2019-04-28 18:17:10 +0800

    0428

---
+++ caoxun/model/factory_tool.py
@@ -0,0 +1,15 @@
+# -*- encoding:utf-8 -*-
+
+class Factory():
+
+    @classmethod
+    def add(cls,name,city,address,contact,tel,worker):
+        #TODO 传入的数组按工厂数据格式进行整理
+        pass
+        # TODO 向现有的工厂数据列表插入新的工厂数据
+        pass
+        # TODO 返回操作结果的状态：是否成功，返回新增工厂的编号
+        # 成功：True, 1001
+        # 失败：False，None
+        return("Factory -> add")
+
