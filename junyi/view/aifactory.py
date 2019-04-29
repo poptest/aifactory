2d2f7a529d528d1dde3cc8b6b3a107a031c5b606
qsong <qsong.vip@qq.com>
2019-04-29 18:07:34 +0800

    上传前端业务代码

--- junyi/view/aifactory.py
+++ junyi/view/aifactory.py
@@ -1,15 +1,39 @@
 # -*- encoding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 from junyi.model.factory import Factory
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route("/factory/add")
+@app.route("/factory/add", methods=['GET', 'POST'])
 def factory_add():
-    return(Factory.add())
+    req_method = request.method
+    print("User Reuqest method: %s" % req_method)
+    if req_method == 'GET':
+        return render_template("factory_add.html")
+    else:
+        no = "00001"
+        name = request.form.get("fac_name")
+        city = request.form.get("city")
+        address = request.form.get("address")
+        contact = request.form.get("contact")
+        tel = request.form.get("tel")
+        dj = request.form.get("dj")
+        status="1"
+        return render_template("factory_add_success.html",
+                               no=no,
+                               name=name,
+                               city=city,
+                               address=address,
+                               contact=contact,
+                               tel=tel,
+                               dj=dj,
+                               status=status)
 
 @app.route("/factory/search/")
 def factory_search_all():
