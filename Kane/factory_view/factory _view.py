0d723d79e564f6deda41aa65403c476cfc0d02c6
KaneQin <352093042@qq.com>
2019-04-29 16:47:54 +0800

    制牙厂管理系统，注册网页

--- Kane/factory_view/factory _view.py
+++ Kane/factory_view/factory _view.py
@@ -1,101 +1,21 @@
 # -*- encoding=utf-8 -*-
 # 创建工厂网页注册页面
 
-from flask import Flask, request
+from flask import Flask, request,render_template
 from Kane.factory_model.factory_model import Factory
 
 app = Flask(__name__)
+
+@app.route("/factory")
+def factory():
+    return render_template("index.html")
 
 
 # 添加工厂的网页
 @app.route("/factory/add",methods=['POST','GET'])
 def factory_add():
     if request.method=="GET":
-
-        html_page = '''
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title></title>
-                </head>
-                <body>
-                    <form action="" method="post" >
-                        <h1 align="center">工厂注册系统</h1>
-                        <table align="center" border="1px" cellspacing="0px" width="50%" height="500px">
-                            <tr>
-                                <td><font color="red">编号</font></td>
-                                <td>
-                                    <input type="number" placeholder ="请输入编号" name="No"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">工厂名称</font></td>
-                                <td>
-                                    <input type="text" placeholder ="请输入工厂名称" name="Facory_Name"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">城市</font></td>
-                                <td>
-                                    <select name="City">
-                                        <option value="武汉">武汉</option>
-                                        <option value="广东">广东</option>
-                                        <option value="北京">北京</option>
-                                        <option value="上海">上海</option>
-                                        <option value="南京">南京</option>
-                                        <option value="福建">福建</option>
-                                        <option value="长沙">长沙</option>
-                                        <option value="深圳">深圳</option>
-                                        <option value="郑州">郑州</option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">地址</font></td>
-                                <td>
-                                    <textarea cols="50" rows="3" maxlength="50" placeholder ="请输入地址" name="Address"></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">联系人</font></td>
-                                <td>
-                                    <input type="text" placeholder ="请输入联系人"  name="Contact"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">联系人电话</font></td>
-                                <td>
-                                    <input type="number" maxlength="11" placeholder ="请输入联系电话" name="Tel"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">对接人</font></td>
-                                <td>
-                                    <input type="text" placeholder ="请输入对接人" name="Worker"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><font color="red">账号状态</font></td>
-                                <td>
-                                    <input type="radio" name="Status"  value="启用"/>启用
-                                    <input type="radio" name="Status"  value="停用" checked/>停用
-                                    <input type="radio" name="Status"  value="注销"/>注销
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2" align="center">
-                                    <input type="submit" value="注册"/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-                                    <input type="reset" value="重置" />&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-                                    <button>登录</button>
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                     
-                </body>
-            </html>
-        '''
-        return (html_page)
+        return render_template("factory.html")
     else:
         No=request.form.get('No')
         Facory_Name=request.form.get('Facory_Name')
@@ -105,9 +25,8 @@ def factory_add():
         Tel=request.form.get('Tel')
         Worker=request.form.get('Worker')
         Status=request.form.get('Status')
-        msg="NO->%s  Facory_Name->%s  City->%s  Address->%s  Contact->%s  Tel->%s  Worker->%s  Status->%s"%(No, Facory_Name, City, Address, Contact, Tel, Worker, Status)
+        msg="NO->%s | Facory_Name->%s | City->%s | Address->%s | Contact->%s | Tel->%s | Worker->%s | Status->%s"%(No, Facory_Name, City, Address, Contact, Tel, Worker, Status)
         return msg
-    # return (Factory.factory_add())
 
 
 # 查询所以工厂信息的网页
